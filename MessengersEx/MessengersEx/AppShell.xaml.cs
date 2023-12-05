c9011ecc39132cb62f4f3b6aaf1b8be6753af649
Gulam Ali H <31090457+FreakyAli@users.noreply.github.com>
2023-12-05 16:54:49 +0530

    fix: initial commit with basic .net 8 proj

---
+++ MessengersEx/MessengersEx/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MessengersEx;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MessengersEx/MessengersEx/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MessengersEx;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
